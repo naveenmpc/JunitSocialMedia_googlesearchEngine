b06b697c16a82c81b37156cfcf084e1ed10c402d
navin <mnaveenkumar1207@gmail.com>
2023-10-24 20:27:53 +0530

    done

---
+++ Phase2/test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"11cbb892-4a91-432a-a978-1c26627e8eb0","feature":"Test the starHealth page on Chrome Browser","scenario":"Validate the Star Health Buy Now flow","start":1698159401453,"group":1,"content":"","tags":"","end":1698159427551,"className":"failed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
